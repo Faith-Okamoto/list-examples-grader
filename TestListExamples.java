2c9ad8a7e0149000d00055ada62b341197e90c00
Faith-Okamoto <114197852+Faith-Okamoto@users.noreply.github.com>
2022-11-09 17:25:02 -0800

    created autograder

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,115 @@
 import static org.junit.Assert.*;
+import java.util.*;
 import org.junit.*;
 
+class DogChecker implements StringChecker {
+    @Override
+    public boolean checkString(String s) {
+        return s.startsWith("dog");
+    }
+}
+
 public class TestListExamples {
-  // Write your grading tests here!
+  @Test
+  public void testFilterEmpty() {
+      List<String> input = new ArrayList<>();
+      StringChecker checker = new DogChecker();
+      assertTrue(ListExamples.filter(input, checker).isEmpty());
+  }
+
+  @Test
+  public void testFilterOneGood() {
+      List<String> input = new ArrayList<>(Arrays.asList("dog1"));
+      StringChecker checker = new DogChecker();
+      assertEquals(new ArrayList<>(Arrays.asList("dog1")),
+                    ListExamples.filter(input, checker));
+  }
+
+  @Test
+  public void testFilterOneBad() {
+      List<String> input = new ArrayList<>(Arrays.asList("cat1"));
+      StringChecker checker = new DogChecker();
+      assertTrue(ListExamples.filter(input, checker).isEmpty());
+  }
+
+  @Test
+  public void testFilterTwoGood() {
+      List<String> input = new ArrayList<>(Arrays.asList("dog1", "dog2"));
+      StringChecker checker = new DogChecker();
+      assertEquals(new ArrayList<>(Arrays.asList("dog1", "dog2")),
+                    ListExamples.filter(input, checker));
+  }
+
+  @Test
+  public void testFilterTwoHalf() {
+      List<String> input = new ArrayList<>(Arrays.asList("dog1", "cat2"));
+      StringChecker checker = new DogChecker();
+      assertEquals(new ArrayList<>(Arrays.asList("dog1")),
+                    ListExamples.filter(input, checker));
+  }
+
+  @Test
+  public void testFilterLong() {
+      List<String> input = new ArrayList<>(
+        Arrays.asList("dog1", "dog2", "cat3", "dog4", "cat5", "cat6")
+      );
+      StringChecker checker = new DogChecker();
+      assertEquals(new ArrayList<>(Arrays.asList("dog1", "dog2", "dog4")),
+                    ListExamples.filter(input, checker));
+  }
+
+  @Test(timeout = 1000)
+  public void testMergeEmpty() {
+      List<String> list1 = new ArrayList<>();
+      List<String> list2 = new ArrayList<>();
+      assertTrue(ListExamples.merge(list1, list2).isEmpty());
+  }
+
+  @Test(timeout = 1000)
+  public void testMergeOnly1() {
+      List<String> list1 = new ArrayList<>(Arrays.asList("hi"));
+      List<String> list2 = new ArrayList<>();
+      assertEquals(new ArrayList<>(Arrays.asList("hi")), 
+                    ListExamples.merge(list1, list2));
+  }
+
+  @Test(timeout = 1000)
+  public void testMergeOnly2() {
+      List<String> list1 = new ArrayList<>();
+      List<String> list2 = new ArrayList<>(Arrays.asList("hi"));
+      assertEquals(new ArrayList<>(Arrays.asList("hi")), 
+                    ListExamples.merge(list1, list2));
+  }
+
+  @Test(timeout = 1000)
+  public void testMergeOnlyOneFirst() {
+      List<String> list1 = new ArrayList<>(Arrays.asList("a"));
+      List<String> list2 = new ArrayList<>(Arrays.asList("b"));
+      assertEquals(new ArrayList<>(Arrays.asList("a", "b")), 
+                    ListExamples.merge(list1, list2));
+  }
+
+  @Test(timeout = 1000)
+  public void testMergeOnlyTwoFirst() {
+    List<String> list1 = new ArrayList<>(Arrays.asList("b"));
+    List<String> list2 = new ArrayList<>(Arrays.asList("a"));
+    assertEquals(new ArrayList<>(Arrays.asList("a", "b")), 
+                  ListExamples.merge(list1, list2));
+  }
+
+  @Test(timeout = 1000)
+  public void testMergeTwoDuplicates() {
+    List<String> list1 = new ArrayList<>(Arrays.asList("a"));
+    List<String> list2 = new ArrayList<>(Arrays.asList("a"));
+    assertEquals(new ArrayList<>(Arrays.asList("a", "a")), 
+                  ListExamples.merge(list1, list2));
+  }
+
+  @Test(timeout = 1000)
+  public void testMergeLong() {
+    List<String> list1 = new ArrayList<>(Arrays.asList("b", "d", "e"));
+    List<String> list2 = new ArrayList<>(Arrays.asList("a", "c", "f"));
+    assertEquals(new ArrayList<>(Arrays.asList("a", "b", "c", "d", "e", "f")), 
+                  ListExamples.merge(list1, list2));
+  }
 }
--- grade.sh
+++ grade.sh
@@ -3,5 +3,67 @@
 set -e
 
 rm -rf student-submission
-git clone $1 student-submission
+git clone --quiet $1 student-submission
 
+cd student-submission
+
+if [[ ! -f ListExamples.java ]]
+then
+    echo "ListExamples.java file not found at top level; score is 0"
+    exit 0
+fi
+
+if [[ $(grep -c "interface StringChecker { boolean checkString(String s); }" ListExamples.java) -eq 0 ]]
+then
+    echo "ListExamples.java does not have the correct StringChecker interface; score is 0"
+    exit 0
+fi
+
+if [[ $(grep -Ec 'static List<String> filter\(List<String>.+, StringChecker.+\)' ListExamples.java) -eq 0 ]]
+then
+    echo "ListExamples.java does not have a filter method with the expected signature; score is 0"
+    exit 0
+fi
+ 
+if [[ $(grep -Ec 'static List<String> merge\(List<String>.+, List<String>.+\)' ListExamples.java) == 0 ]]
+then
+    echo "ListExamples.java does not have a merge method with the expected signature; score is 0"
+    exit 0
+fi
+
+cp ../TestListExamples.java ./
+
+set +e
+javac ListExamples.java 2> error.txt
+EXIT=$?
+set -e
+
+if [[ $EXIT -ne 0 ]]
+then
+    echo "ListExamples.java failed to compile (error with code $EXIT); score is 0."
+    exit 0
+fi
+
+if [[ ! -f ListExamples.class ]]
+then
+    echo "ListExamples class not found; score is 0"
+    exit 0
+fi
+
+CP=".;../lib/hamcrest-core-1.3.jar;../lib/junit-4.13.2.jar" 
+javac -cp $CP TestListExamples.java
+
+set +e
+java -cp $CP org.junit.runner.JUnitCore TestListExamples > test.txt
+set -e
+
+if [[ $(grep -c "^OK" test.txt) -eq 1 ]]
+then
+    TESTS=$(grep -Po "\d+ test" test.txt | grep -Po "\d+")
+    echo "All tests passed; score is ${TESTS}"
+else
+    TESTS=$(grep -Po "Tests run: \d+" test.txt | grep -Po "\d+")
+    FAILED=$(grep -Po "Failures: \d+" test.txt | grep -Po "\d+")
+    PASSED=$((TESTS - FAILED))
+    echo "${PASSED} out of ${TESTS} passed; score is ${PASSED}."
+fi
