6236c72a1c872dba4a3bea4fbf4264d4293a86a7
Faith-Okamoto <114197852+Faith-Okamoto@users.noreply.github.com>
2022-11-16 14:30:03 -0800

    notify of operating system discrepancy

--- grade.sh
+++ grade.sh
@@ -1,3 +1,5 @@
+# THE CLASSPATH ASSUMES A WINDOWS SYSTEM: IF ON MAC CHANGE LINE 54 TO USE .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar
+
 # make script quit if it has an error - mostly for debugging purposes
 set -e
 
